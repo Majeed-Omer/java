38be3b61e4c88764617674ad472fc53b2f8eb218
Majeed-Omer <91630712+Majeed-Omer@users.noreply.github.com>
2021-10-11 22:04:07 +0300

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -2,7 +2,44 @@ package m;
 import java.util.Scanner;
 
 public class Zoo {
+	static String email = "";
+	static String password = "";
     public static void main(String args[]) {
+    	EP();
+    }
+    	
+	public static void display_options1(){
+        System.out.println("Options:");
+        System.out.println("1- animal - show you list of sub actions with animals");
+        System.out.println("2- employee - show you list of sub actions with employees");
+        System.out.println("3- supply - show you list of sub actions with supplies");
+        System.out.println("4- income - show you list of sub actions with income");
+        System.out.println("4- expenditure - show you list of sub actions with expenditure");
+        System.out.println("5- ticket - show list of sub actions with tickets");
+        System.out.println("6- back - back to email and password please");
+
+        System.out.println("--");
+        System.out.println("options - display this options message");
+        System.out.println("exit - exit the program\n");
+    }
+	public static void display_options2(){
+        System.out.println("Options:");
+        System.out.println("1- animal - show you list of sub actions with animals");
+        System.out.println("2- ticket - show list of sub actions with tickets");
+        System.out.println("3- back - back to email and password please");
+
+        System.out.println("--");
+        System.out.println("options - display this options message");
+        System.out.println("exit - exit the program\n");
+    }
+	public static void EP(){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter email and password:");
+    	System.out.print("Email: ");
+    	email = s.nextLine();
+    	System.out.print("Password: ");
+    	password = s.nextLine();
+    	System.out.println();
     	animal a = new animal();
     	employee e = new employee();
     	supply sp = new supply();
@@ -10,16 +47,7 @@ public class Zoo {
     	expenditure ex = new expenditure();
     	ticket t = new ticket();
     	login l = new login();
-    	Scanner s = new Scanner(System.in);
     	String actions = "";
-    	String email = "";
-    	String password = "";
-    	System.out.println("Please enter email and password:");
-    	System.out.print("Email: ");
-    	email = s.nextLine();
-    	System.out.print("Password: ");
-    	password = s.nextLine();
-    	System.out.println();
     	if(email.equals("muhamad23@gmail.com") && password.equals("12345!@#$%")) {
     		while(!actions.equals("exit")) {
         		display_options1();
@@ -45,6 +73,9 @@ public class Zoo {
         	case "ticket":
                 t.publicActions();
             break;
+        	case "back":
+                EP();
+            break;
         	case "exit":
                 System.exit(0);
             break;
@@ -66,6 +97,9 @@ public class Zoo {
             case "ticket":
             t.publicActions();
             break;
+            case "back":
+                EP();
+            break;
             case "exit":
                 System.exit(0);
             break;
@@ -75,27 +109,5 @@ public class Zoo {
     }
     }
     		
-    }
-	public static void display_options1(){
-        System.out.println("Options:");
-        System.out.println("1- aniaml - show you list of sub actions with animals");
-        System.out.println("2- employee - show you list of sub actions with employees");
-        System.out.println("3- supply - show you list of sub actions with supplies");
-        System.out.println("4- income - show you list of sub actions with income");
-        System.out.println("4- expenditure - show you list of sub actions with expenditure");
-        System.out.println("5- ticket - show list of sub actions with tickets");
-
-        System.out.println("--");
-        System.out.println("options - display this options message");
-        System.out.println("exit - exit the program\n");
-    }
-	public static void display_options2(){
-        System.out.println("Options:");
-        System.out.println("1- aniaml - show you list of sub actions with animals");
-        System.out.println("2- ticket - show list of sub actions with tickets");
-
-        System.out.println("--");
-        System.out.println("options - display this options message");
-        System.out.println("exit - exit the program\n");
-    }
+	}
 }
